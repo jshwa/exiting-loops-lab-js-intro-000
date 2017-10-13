ea075a9f0e74f561ba8fd0789ef7525dbcc72e16
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-13 18:17:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === stopValue){
+    if (array[i] != stopValue){
+      array[i] = changeValue
+    } else {
       break
-    }
-    array[i] = changeValue
+    }  
   }
 }
